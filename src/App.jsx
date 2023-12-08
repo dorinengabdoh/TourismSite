44cbe198d3ffefd6b87a6e714b7d095b6d4e2a93
dorinengabdoh <ngabdohdorine@gmail.com>
2023-12-08 04:21:47 +0100

    first commit on main branch

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Gallery from './assets/Component/GaleryComponent/Gallery'
+
+function App() {
+
+  return (
+    <>
+    <Gallery/>
+      
+    </>
+  )
+}
+
+export default App
